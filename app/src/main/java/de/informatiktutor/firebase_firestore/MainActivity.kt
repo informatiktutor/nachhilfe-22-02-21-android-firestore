6e6b90cbbe6d544b4494d129a1f2f8d4dcfcd739
vonas <jonas@informatiktutor.de>
2022-02-21 19:38:38 +0100

    Initial commit

---
+++ app/src/main/java/de/informatiktutor/firebase_firestore/MainActivity.kt
@@ -0,0 +1,18 @@
+package de.informatiktutor.firebase_firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.informatiktutor.firebase_firestore.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/de/informatiktutor/firebase_firestore/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package de.informatiktutor.firebase_firestore.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
